ae5a437812f61c99d8e8faebefbe21e80a544304
atbj505 <atbj505@hotmail.com>
2015-02-27 10:00:57 +0800

    project init

---
+++ TQRichTextViewTest/TQRichTextViewTest/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  TQRichTextViewTest
+//
+//  Created by 杨启晖 on 15/2/27.
+//  Copyright (c) 2015年 NationSky. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "ViewController.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    ViewController *vc = [[ViewController alloc]init];
+    self.window.rootViewController = vc;
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end
